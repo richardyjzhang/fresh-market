8e3c4d6fcb41c3006bbeeb0bb01d4e235dd40be5
ZhangYJ <richard.yujia.zhang@gmail.com>
2024-02-19 23:25:07 +0800

    fix: 改正SQL中的错误

--- server-side/fresh-market/src/main/java/com/zhangrichard/freshmarket/security/LoginMapper.java
+++ server-side/fresh-market/src/main/java/com/zhangrichard/freshmarket/security/LoginMapper.java
@@ -7,8 +7,8 @@ import org.apache.ibatis.annotations.Select;
 @Mapper
 public interface LoginMapper {
 
-    @Select("SELECT id, loginName " +
+    @Select("SELECT id, loginName, " +
             "password, salt FROM user " +
-            "WHERE u.loginName = #{loginName} ")
+            "WHERE loginName = #{loginName} ")
     LoginUser getUserByLoginName(String loginName);
 }
