8ac78680f252cd85b1f12cd086dcd75d15b493c4
ZhangYJ <richard.yujia.zhang@gmail.com>
2024-02-21 20:01:40 +0800

    feat: 后台布局中头部样式

--- web-side/src/layouts/management/index.tsx
+++ web-side/src/layouts/management/index.tsx
@@ -1,10 +1,48 @@
 import { Outlet } from 'umi';
+import { ConfigProvider, Dropdown } from 'antd';
 import styles from './index.css';
 
 export default function Layout() {
+
+    // 右上角用户名的下拉菜单选项
+    const userMenuItems = [
+        {
+            key: 'editPassword',
+            label: '修改密码',
+            onClick: () => {
+                console.log('TODO: 修改密码');
+            }
+        },
+        {
+            key: 'logout',
+            label: '退出登录',
+            onClick: () => {
+                console.log('退出登录');
+            }
+        }
+    ];
+
     return (
-        <div className={styles.navs}>
-            <Outlet />
-        </div>
+        <ConfigProvider theme={{
+            token: {
+                colorPrimary: '#00b96b',
+            }
+        }}>
+            <div className={styles.root}>
+                <nav className={styles.nav}>
+                    <img className={styles.icon} src='icon.png' />
+                    <div className={styles.title}>瑶总鲜选管理后台</div>
+                    <div className={styles.space} />
+                    <div>
+                        <Dropdown menu={{ items: userMenuItems, }}>
+                            <div className={styles.userName}>管理员</div>
+                        </Dropdown>
+                    </div>
+
+                </nav>
+                <Outlet />
+            </div>
+        </ConfigProvider>
+
     );
 }
