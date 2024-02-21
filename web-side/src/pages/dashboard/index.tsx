1faba298a08bf5166162c5b46f2731f347a61045
ZhangYJ <richard.yujia.zhang@gmail.com>
2024-02-21 21:33:34 +0800

    feat: 临时汇报用经营概览

--- web-side/src/pages/dashboard/index.tsx
+++ web-side/src/pages/dashboard/index.tsx
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
+import styles from './index.css';
 
 const DashboardPage: React.FC = () => {
-    return <div>经营概览</div>
+    const countupRef = useRef(null);
+    let countUpAnim;
+
+    useEffect(() => {
+        initCountUp();
+    }, []);
+
+    async function initCountUp() {
+        const countUpModule = await import('countup.js');
+        countUpAnim = new countUpModule.CountUp(countupRef.current, 1000);
+        if (!countUpAnim.error) {
+            countUpAnim.start();
+        } else {
+            console.error(countUpAnim.error);
+        }
+    }
+
+    return (<div className={styles.root}>累计收入 <div ref={countupRef}></div> 万元</div>);
 }
 
 export default DashboardPage;
